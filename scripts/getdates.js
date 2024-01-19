02ac0b86edf9e7839368f3a3c47a1bb40f095b91
Emily Cordero <cordeemi031@gmail.com>
2024-01-19 02:41:57 -0800

    added home page

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const year = document.getElementById("year");
+const lastModified = document.getElementById("lastModified");
+const d = new Date()
+let currentYear = d.getFullYear();
+
+document.getElementById("year").innerHTML = currentYear;
+document.getElementById("lastModified").innerHTML = Date()
+
